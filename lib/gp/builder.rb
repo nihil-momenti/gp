4a643c13953171af562a8d0dc2298a0dce60b0fe
Wim Looman <ghostunderscore@gmail.com>
2011-04-16 00:09:43 +1200

    Moved regex into the module properly and fixed the code regex for nested parantheses

--- lib/gp/builder.rb
+++ lib/gp/builder.rb
@@ -3,21 +3,21 @@ require 'yaml'
 require File.join File.dirname(__FILE__), 'function'
 
 module GP
+  name = /(\w+)/
+  name_sep = /\s*:\s*/
+  args = /((?:\s*\w+\s*,\s*)*\s*\w+)/
+  type_sep = /\s*->\s*/
+  type = /(\w+)/
+  code_sep = /\s*=>\s*/
+  code = /(\(.*\))/
+
+  FORMULA_REGEX = /#{name}#{name_sep}#{args}#{type_sep}#{type}#{code_sep}#{code}/
+
   class << self
     def parse_file file
       yaml = YAML.parse_file file
       functions = parse_formulas yaml['functions']
     end
-
-    name = /(\w+)/
-    name_sep = /\s*:\s*/
-    args = /((?:\s*\w+\s*,\s*)*\s*\w+)/
-    type_sep = /\s*->\s*/
-    type = /(\w+)/
-    code_sep = /\s*=>\s*/
-    code = /(\(.*?\))/
-
-    FORMULA_REGEX = /#{name}#{name_sep}#{args}#{type_sep}#{type}#{code_sep}#{code}/
 
     def parse_formulas s
       s.scan(FORMULA_REGEX).map do |name, args, type, code|
