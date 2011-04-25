e830ee7bf67c50aebe586835fc3307426c0101f9
Wim Looman <ghostunderscore@gmail.com>
2011-04-25 22:28:32 +1200

    Added parsing of constants and variables.  Renamed function parsing method

--- lib/gp/builder.rb
+++ lib/gp/builder.rb
@@ -16,16 +16,26 @@ module GP
   class << self
     def parse_file file
       yaml = YAML.parse_file file
-      functions = parse_formulas yaml['functions']
+      functions = parse_functions yaml['functions']
+      constants = parse_constants yaml['constants']
+      variables = parse_variables yaml['variables']
     end
 
-    def parse_formulas s
+    def parse_functions s
       s.scan(FORMULA_REGEX).map do |name, args, type, code|
         name = name.to_sym
         args = args.scan(/\w+/).map(&:to_sym)
         type = type.to_sym
         Function.new name, args, type, code
       end
+    end
+
+    def parse_constants h
+      Hash[ h.map { |k, v| [k.to_sym, eval "proc { #{v} }"] } ]
+    end
+
+    def parse_variables h
+      Hash[ h.map { |k, v| [k, v.to_sym] } ]
     end
   end
 end
