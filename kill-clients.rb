13841ec36d0f3f6fad3d7caeadddd143c0de52c1
Wim Looman <ghostunderscore@gmail.com>
2011-05-20 23:17:18 +1200

    Updating the process control scripts

--- kill-clients.rb
+++ kill-clients.rb
@@ -3,6 +3,6 @@
 hosts = `cat /netfs/share/whichprinter/csse_lab?  | sed 's/$/.cosc.canterbury.ac.nz 22/' | xargs -n2 nc -w 2 -z -v 2> /dev/null | egrep -o "cosc[0-9]+\.cosc\.canterbury\.ac\.nz"`.split("\n")
 hosts << 'localhost'
 hosts.each do |host|
-  puts "Shutting down #{host}"
+  puts "Killing workers on #{host}"
   puts `ssh -o ConnectTimeout=2 #{host} 'ps -e -o pid,cmd | grep resque | grep -v web | grep -v grep | grep -Eo "^\s*[[:digit:]]+" | grep -Eo "[[:digit:]]+" | xargs -r kill'`
 end
--- spawn-clients.rb
+++ spawn-clients.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 hosts = `cat /netfs/share/whichprinter/csse_lab?  | sed 's/$/.cosc.canterbury.ac.nz 22/' | xargs -n2 nc -w 2 -z -v 2> /dev/null | egrep -o "cosc[0-9]+\.cosc\.canterbury\.ac\.nz"`.split("\n")
+hosts << 'localhost'
 hosts.each do |host|
   puts "Spawning on #{host}"
   puts `ssh -o ConnectTimeout=2 #{host} ~/.rvm/wrappers/ruby-1.9.2-p180/ruby ~/sources/gp/run-clients.rb`
